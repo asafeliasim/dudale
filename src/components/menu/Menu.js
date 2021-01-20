a3b3774720e57685b296e6a5a06a9f2ac137b5d6
unknown <asafeliasim@gmail.com>
2021-01-20 17:18:50 +0200

    fix menu to mobile

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -13,18 +13,20 @@ const style = {
     textDecoration:'none',
    
 }
-
-
+const mobile = window.innerWidth <= 960 ? true : false;
+const mobileStyle = {
+    marginTop: '-8rem'
+}
 const Menu = () => {
     return (
-        <section className="menu" id="productsection">
+        <section className="menu" id="productsection" style={mobile ? mobileStyle : null}>
             <div className="headline d-flex justify-content-center">
                <img src={productTitle} alt="" className="menu_title-img"/>
             </div>
             
             <div className="container products">
                 <div className="row mx-auto">
-                    <div className="col-12 col-md-4 my-5">
+                    <div className="col-6 col-md-4 my-5">
                         <LinkContainer to="/creams" style={style}>  
                             <div className="box">
                                 <div className="box_img">
@@ -33,7 +35,7 @@ const Menu = () => {
                             </div> 
                         </LinkContainer>
                     </div>
-                    <div className="col-12 col-md-4 my-5">
+                    <div className="col-6 col-md-4 my-5">
                     <LinkContainer to="/icecreams" style={style}>  
                     <div className="box">
                         <div className="box_img">
@@ -43,7 +45,7 @@ const Menu = () => {
                     </div> 
                     </LinkContainer>
                     </div>
-                    <div className="col-12 col-md-4 my-5">
+                    <div className="col-6 col-md-4 my-5">
                         <LinkContainer to="/snacks" >  
                             <div className="box">
                                 <div className="box_img" style={{height:'24rem!important'}}>
@@ -53,7 +55,7 @@ const Menu = () => {
                         </LinkContainer>
                     </div>
                 
-                    <div className="col-12 col-md-4 my-5">
+                    <div className="col-6 col-md-4 my-5">
                     <LinkContainer to="/chocolats/#products" style={style}>  
                     <div className="box">
                         <div className="box_img">
@@ -63,7 +65,7 @@ const Menu = () => {
                     </div> 
                     </LinkContainer>
                     </div>
-                    <div className="col-12 col-md-4 my-5">
+                    <div className="col-6 col-md-4 my-5">
                     <LinkContainer to="/drinks" style={style}>  
                     <div className="box">
                         <div className="box_img">
@@ -72,7 +74,7 @@ const Menu = () => {
                     </div> 
                     </LinkContainer>
                     </div>
-                    <div className="col-12 col-md-4 my-5">
+                    <div className="col-6 col-md-4 my-5">
                         <LinkContainer to="/fruits" style={style}>  
                         <div className="box">
                             <div className="box_img">
